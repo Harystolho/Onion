8fa964172170125efb12c422d4c1c7b616a098d7
Harystolho <haristolhogamer@gmail.com>
2020-11-16 09:11:35 -0300

    Criar pagina inicial com bootstrap e jquery

--- Onion/Startup.cs
+++ Onion/Startup.cs
@@ -46,7 +46,7 @@ namespace Onion
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Usuario}/{action=Index}");
             });
         }
     }
